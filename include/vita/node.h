8a7f8206ce31111d9c080b35d10eb04569c8ccdb
Jimmy Backend <jimmybackend@gmail.com>
2026-04-23 14:30:49 -0600

    Refactor vita_node_peer_t and add vita_node_task_t

--- include/vita/node.h
+++ include/vita/node.h
@@ -6,15 +6,34 @@
 
 #include <vita/boot.h>
 
+#define VITA_NODE_ID_MAX 64
+#define VITA_NODE_TRANSPORT_MAX 32
+#define VITA_NODE_STATE_MAX 32
+#define VITA_NODE_CAPABILITIES_JSON_MAX 256
+#define VITA_NODE_TASK_TYPE_MAX 64
+#define VITA_NODE_TASK_STATUS_MAX 32
+#define VITA_NODE_TASK_PAYLOAD_JSON_MAX 512
+
 typedef struct {
-    char peer_id[64];
+    char peer_id[VITA_NODE_ID_MAX];
     uint64_t first_seen_unix;
     uint64_t last_seen_unix;
-    char transport[32];
-    char capabilities_json[256];
-    char trust_state[32];
-    char link_state[32];
+    char transport[VITA_NODE_TRANSPORT_MAX];
+    char capabilities_json[VITA_NODE_CAPABILITIES_JSON_MAX];
+    char trust_state[VITA_NODE_STATE_MAX];
+    char link_state[VITA_NODE_STATE_MAX];
 } vita_node_peer_t;
+
+typedef struct {
+    char task_id[VITA_NODE_ID_MAX];
+    char origin_node_id[VITA_NODE_ID_MAX];
+    char target_node_id[VITA_NODE_ID_MAX];
+    char task_type[VITA_NODE_TASK_TYPE_MAX];
+    char status[VITA_NODE_TASK_STATUS_MAX];
+    uint64_t created_unix;
+    uint64_t updated_unix;
+    char payload_json[VITA_NODE_TASK_PAYLOAD_JSON_MAX];
+} vita_node_task_t;
 
 bool node_core_start(const vita_handoff_t *handoff);
 int node_core_peer_count(void);
